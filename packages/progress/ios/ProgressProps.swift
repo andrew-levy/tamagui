0765e709a1aad0dcf42985f69f5924d30dc1949b
Andrew <alevy97@gmail.com>
2024-01-28 17:26:02 -0500

    update prog props

--- packages/progress/ios/ProgressProps.swift
+++ packages/progress/ios/ProgressProps.swift
@@ -5,9 +5,5 @@ class ProgressProps: ObservableObject {
   @Published var value: Double = 0
   @Published var max: Double = 1
   @Published var accent: UIColor?
-  @Published var onSized: EventDispatcher
-  init(onSized: EventDispatcher) {
-    self.onSized = onSized
-  }
 }
 
